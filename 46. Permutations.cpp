1166043344fef4faebfdc56e1c4e05b8606abddd
Sofariu Adrian-Radu <66952373+AdrianSofariu@users.noreply.github.com>
2024-09-07 13:03:34 +0300

    Update 46. Permutations.cpp

--- 46. Permutations.cpp
+++ 46. Permutations.cpp
@@ -23,3 +23,37 @@ Constraints:
 -10 <= nums[i] <= 10
 All the integers of nums are unique.
 */
+
+class Solution {
+private:
+    
+    void solve(vector<vector<int>> &ans, vector<int> &nums, int index){
+        //if the current index is equal to the size of nums, we have a solution
+        if(index == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        //iterate over all remaining elements
+        for(int j = index; j < nums.size(); j++){
+            //swap current with element at position j
+            swap(nums[index], nums[j]);
+            //recursively generate permutations for next index
+            solve(ans, nums, index + 1);
+            //revert swap
+            swap(nums[j], nums[index]);
+        }
+    }
+
+    
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        //We will do a backtracking algorithm
+        //Complexity: O(n!) - time, O(n) - space
+        vector<vector<int>> ans;
+        int index = 0;
+        solve(ans, nums, index);
+        return ans;
+    }
+};
