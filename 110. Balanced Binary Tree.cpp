304df75adf4e986c682c93fa890d14f973e2a060
Sofariu Adrian-Radu <66952373+AdrianSofariu@users.noreply.github.com>
2025-02-17 14:50:47 +0200

    Create 110. Balanced Binary Tree.cpp

---
+++ 110. Balanced Binary Tree.cpp
@@ -0,0 +1,48 @@
+/*
+Given a binary tree, determine if it is height-balanced.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+
+Example 3:
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-10^4 <= Node.val <= 10^4
+
+
+*/
+//Complexity: O(n) - time, O(n) - space
+class Solution {
+private:
+
+    int height(TreeNode* root, int& diff){
+
+        if(root == NULL)
+            return 0;
+        
+        int left = height(root->left, diff);
+        int right = height(root->right, diff);
+
+        diff = max(diff, abs(left-right));
+
+        return max(left, right) + 1;
+    }
+
+
+public:
+    bool isBalanced(TreeNode* root) {
+        int diff = 0;
+        height(root, diff);
+        return diff <= 1;
+    }
+};
